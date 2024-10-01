0a04b09aa058deefb2c94bfac73e22a4b842f2b8
viorieeeen <vioomiawwww@gmail.com>
2024-10-01 17:19:45 +0700

    Add files via upload

---
+++ Menentukan Hasil Kelulusan Mahasiswa.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int nilai;
+cout<<">>Program Sederhana Menentukan Kelulusan<<\n\n";
+cout<<"Masukkan Nilainya: ";
+cin>>nilai;
+if (nilai<75){
+cout<<"\nAnda Tidak Lulus\n";
+} else {
+cout<<"\nSelamat Anda Lulus\n";
+}
+return 0;
+}
+
